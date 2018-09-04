c5dea275565f0db06d630f0258fb2013538ff84b
John Hosmer <john.n.hosmer@gmail.com>
2018-09-04 15:40:04 -0700

    Start first test in repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let finalString = "";
+    for (var i = 0; i < times; i++) {
+        finalString += string;
+    }
+    return console.log(finalString);
 }
 
 module.exports = repeatString
