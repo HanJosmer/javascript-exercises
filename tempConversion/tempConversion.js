0da48f54be2d3d1ad640ffc25886a5938abde351
John Hosmer <john.n.hosmer@gmail.com>
2018-09-05 11:03:21 -0700

    Create index.html, complete tempConversion.js, enable all tests

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp - 32) / 1.8;
+  return +temp.toFixed(1);
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = (temp * 1.8) + 32
+  return +temp.toFixed(1);
 }
 
 module.exports = {
