d0ca76111fba81a5e9a89b93e039caf873554c63
John Hosmer <john.n.hosmer@gmail.com>
2018-09-05 09:24:54 -0700

    Fix removeFromArray.js, enable all tests

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,29 +1,18 @@
 const removeFromArray = function() {
     args = Array.from(arguments);
-    inputArray = args[0];
-    outputArray = [];
-    let i = 0;
-    
-    while (i < inputArray.length) {
-        for (let j = 1; j < arguments.length; j++) {
-            if (inputArray[i] == arguments[j]) {
-                outputArray.push()
-            } 
+    let inputArray = args[0];
+    let length = inputArray.length;
+
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j < length; j++) {
+            if (arguments[i] === inputArray[j]) {
+                inputArray.splice(j, 1);
+                length = inputArray.length;
+                i--;  // account for element shift after splicing
+            }
         }
-        i++;
-    }
-
+    } 
     return inputArray;
-
-    // for (let i = 0; i < inputArray.length; i++) {
-    //     for (let j = 1; j < arguments.length; j++) {
-    //         if (inputArray[i] == arguments[j]) {
-    //             inputArray.splice(inputArray[i-1], 1);
-    //             i++;
-    //         }
-    //     }
-    // }
-    // return inputArray;
 }
 
 module.exports = removeFromArray
