5f6e22f9c3b30e3f4209fbc3242f1967e11047c4
John Hosmer <john.n.hosmer@gmail.com>
2018-09-05 09:45:35 -0700

    Create index.html, complete sumAll.js, enable all tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(lowerBound, upperBound) {
+    
+    // return "ERROR" if negative or NaN
+    if (typeof lowerBound != "number" || typeof upperBound != "number") {
+        return "ERROR";
+    } else if (lowerBound < 0 || upperBound < 0) {
+        return "ERROR";
+    }
 
+    // swap upper and lower bounds if larger number first
+    if (lowerBound > upperBound) {
+        let temp = lowerBound;
+        lowerBound = upperBound;
+        upperBound = temp;
+    }
+    
+    let sum = 0;
+    for (let i = lowerBound; i <= upperBound; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
 module.exports = sumAll
