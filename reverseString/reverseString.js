8eb32243f9e97fb93961c95bec91424547d26ca6
John Hosmer <john.n.hosmer@gmail.com>
2018-09-04 17:24:12 -0700

    Complete reverseString.js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let outputString = "";
+    for (i = 0; i < inputString.length; i++) {
+        outputString += inputString[(inputString.length - 1) - i];
+    }
+    return outputString;
 }
 
 module.exports = reverseString
