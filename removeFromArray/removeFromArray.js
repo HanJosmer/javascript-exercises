45404abbbc1bdbd522cff32b52bc88878e7deec8
John Hosmer <john.n.hosmer@gmail.com>
2018-09-04 19:24:28 -0700

    Create index.html, begin removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
 const removeFromArray = function() {
+    args = Array.from(arguments);
+    inputArray = args[0];
+    outputArray = [];
+    let i = 0;
+    
+    while (i < inputArray.length) {
+        for (let j = 1; j < arguments.length; j++) {
+            if (inputArray[i] == arguments[j]) {
+                outputArray.push()
+            } 
+        }
+        i++;
+    }
 
+    return inputArray;
+
+    // for (let i = 0; i < inputArray.length; i++) {
+    //     for (let j = 1; j < arguments.length; j++) {
+    //         if (inputArray[i] == arguments[j]) {
+    //             inputArray.splice(inputArray[i-1], 1);
+    //             i++;
+    //         }
+    //     }
+    // }
+    // return inputArray;
 }
 
 module.exports = removeFromArray
