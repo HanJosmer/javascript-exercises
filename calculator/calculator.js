d684b4532f03fa1cbf2074bcdab1e29d83d9e139
John Hosmer <john.n.hosmer@gmail.com>
2018-09-20 13:33:45 -0700

    Complete calculator.js and palindromes.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,50 @@
-function add () {
-	
+function add (x, y) {
+	// return (typeof x === number && typeof y === number) ? x + y : alert("Error: non-numeric input");
+	return x + y;
 }
 
-function subtract () {
-	
-}
+function subtract (x, y) {
+	// return (typeof x === number && typeof y === number) ? x - y : alert("Error: non-numeric input");
+	return x - y;
+};
 
-function sum () {
-	
-}
+function sum (array) {
+	const sum = array.reduce((total, item) => {
+		return total + item;
+	}, 0);
+	return sum;
+};
 
-function multiply () {
-	
-}
+function multiply (array) {
+	const product = array.reduce((product, factor) => {
+		return product * factor;
+	}, 1);
+	return product;
+};
 
-function power() {
-	
-}
+function power(x, y) {
+	let product = 1;
+	if (y === 0) {return 1}
+	else if (y < 0) {return alert("Negative powers are unsupported at this time")}
+	else {
+		for (let i = 0; i < y; i++) {
+			product *= x;
+		};
+	};
+	return product;
+};
 
-function factorial() {
-	
-}
+function factorial(x) {
+	let factorial = 1;
+	if (x === 0 || x === 1) {
+		return 1;
+	} else {
+		for (let i = x; i > 1; i--) {
+			factorial *= i;
+		};
+	};
+	return factorial;
+};
 
 module.exports = {
 	add,
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,36 @@
-const palindromes = function() {
+const palindromes = function(inputString) {
 
+    // remove punctuation
+    const noPunctuation = inputString.split('').filter((ch) => {
+        if ((ch >= "a" && ch <= "z") || (ch >= "A" && ch <= "Z")) {
+            return true;
+        }
+    });
+    
+    // set chars to lowercase
+    const lowerCase = noPunctuation.map(ch => {
+        return ch.toLowerCase();
+    })
+
+    // reverse santized string
+    const reversed = [];
+    for (let i = lowerCase.length; i > 0; i--) {
+        reversed.push(lowerCase[i - 1]);
+    }
+
+    // compare strings
+    let palindrome = true;
+    let i = 0;
+    while (i < (lowerCase.length - 1)) {
+        if (lowerCase[i] !== reversed[i]) {
+            palindrome = false;
+            break;
+        }
+        i++;
+    };
+
+    // return result
+    console.log(palindrome);
 }
 
-module.exports = palindromes
+// module.exports = palindromes
