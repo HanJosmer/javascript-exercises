3bfd018e8b102bba0eac9860c0640b5d5bfaebdd
John Hosmer <john.n.hosmer@gmail.com>
2018-09-21 13:32:03 -0700

    Create firstConsonants function in pigLatin.js

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,9 +1,39 @@
-function translate() {
-	// body...
+const CONSONANTS = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "z"];
+const VOWELS = ["a", "e", "i", "o", "u"];
+
+function translate(inputString) {
+	
+	// break string into words
+	const words = inputString.split(" ");
+	console.log(words);
+
+	// remove first consonanants from string
+	let j = 0;
+	const first = firstConsonants(words, j);
+	
+	for (let i = 0; i < first.length; i++) {
+		words.shift();
+	}
+
+	console.log(words);
+
+	// determine if word begins with vowel or consonant
+	// todo
+}
+
+function firstConsonants(inputString, j) {
+	const inputStringArray = inputString[j].split("");
+	const consonants = [];
+	let i = 0;
+	while (CONSONANTS.includes(inputStringArray[i])) {
+		consonants.push(inputStringArray[i]);
+		i++;
+	}
+	return consonants;
 }
 
 
-module.exports = {
-	translate
-}
+// module.exports = {
+// 	translate
+// }
 
