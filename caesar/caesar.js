d467f44bef6129319e78b50dd78a5eef7f39b0bd
John Hosmer <john.n.hosmer@gmail.com>
2018-09-21 09:58:15 -0700

    Complete fibonacci.js

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,34 @@
-const caesar = function() {
-
+const caesar = function(inputString, salt) {
+    const inputArray = inputString.split('');
+    console.log(inputArray.join(""));
+    const shifted = inputArray.map(ch => {
+        if (ch >= "A" && ch <= "Z") {
+            const currentCode = ch.charCodeAt(0);
+            const standardizedCode = currentCode - 64;
+            const newStandardizedCode = (standardizedCode + salt) % 26;
+            const newCode = String.fromCharCode(newStandardizedCode + 64);
+            return newCode;
+        } else if (ch >= "a" && ch <= "z") {
+            const currentCode = ch.charCodeAt(0);
+            const standardizedCode = currentCode - 96;
+            const newStandardizedCode = (standardizedCode + salt) % 26;
+            const newCode = String.fromCharCode(newStandardizedCode + 96);
+            return newCode;
+        } else {
+            return ch;
+        }
+    });
+    console.log(shifted.join(""));
 }
 
-module.exports = caesar
+// module.exports = caesar
+
+
+// String.fromCharCode()
+// String.charCodeAt()
+
+// uppercase "A" = 65
+// uppercase "Z" = 90
+
+// lowercase "a" = 97
+// lowercase "z" = 122
--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,36 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
 
+    // check that strings are numeric
+    if (typeof input == "string" && isNumericString(input) === false) {return alert("String cannot be converted to numeric value")};
+
+    // check for negative numbers
+    if (input <= 0) {return "OOPS"};
+
+    // build fibonacci array
+    const fibArray = [];
+    for (let i = 0; i < input; i++) {
+        if (i === 0) {
+            fibArray.push(1);
+        } else if (i === 1) {
+            fibArray.push(1);
+        } else {
+            fibArray.push(fibArray[i - 2] + fibArray[i - 1]);
+        }
+    }
+    
+    console.log(fibArray[fibArray.length - 1]);
+}
+
+function isNumericString(inputString) {
+
+    // if string, check that all characters are numeric
+    const isNumericString = inputString.split('').every(ch => {
+        if (ch >= "0" && ch <= "9") {
+            return true;
+        }
+    });
+    
+    return isNumericString;
 }
 
 module.exports = fibonacci
