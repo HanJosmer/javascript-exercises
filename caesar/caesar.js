7dae417ee9ca5ef1746e5d459f34eb1155938e34
John Hosmer <john.n.hosmer@gmail.com>
2018-09-24 18:46:50 -0700

    Finish Caesar.js

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,24 +1,31 @@
 const caesar = function(inputString, salt) {
     const inputArray = inputString.split('');
     console.log(inputArray.join(""));
+
+    // account for a negative shift
+    while (salt < 0) {
+        salt += 26;
+    }
+
+    // perform shift
     const shifted = inputArray.map(ch => {
         if (ch >= "A" && ch <= "Z") {
             const currentCode = ch.charCodeAt(0);
-            const standardizedCode = currentCode - 64;
+            const standardizedCode = currentCode - 65;
             const newStandardizedCode = (standardizedCode + salt) % 26;
-            const newCode = String.fromCharCode(newStandardizedCode + 64);
+            const newCode = String.fromCharCode(newStandardizedCode + 65);
             return newCode;
         } else if (ch >= "a" && ch <= "z") {
             const currentCode = ch.charCodeAt(0);
-            const standardizedCode = currentCode - 96;
+            const standardizedCode = currentCode - 97;
             const newStandardizedCode = (standardizedCode + salt) % 26;
-            const newCode = String.fromCharCode(newStandardizedCode + 96);
+            const newCode = String.fromCharCode(newStandardizedCode + 97);
             return newCode;
         } else {
             return ch;
         }
     });
-    console.log(shifted.join(""));
+    return shifted.join("");
 }
 
 // module.exports = caesar
